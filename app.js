0c8c36038496b843bd5619f03522013197251624
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-07-24 17:48:23 -0700

    working quiz

--- app.js
+++ app.js
@@ -1,1 +1,103 @@
 'use strict';
+
+function Image(name) {
+  this.name = name;
+  this.source = 'img/' + this.name + '.jpg';
+  this.timesShown = 0;
+  this.timesClicked = 0;
+  Image.all.push(this);
+}
+
+Image.prototype.randomIndex = function() {
+  var randomIndex = Math.floor(Math.random() * Image.all.length);
+  return randomIndex;
+}
+
+Image.imgClicks = [];
+Image.numQuestionsAnswered = 0;
+Image.names = [document.getElementById('imgOne'), document.getElementById('imgTwo'), document.getElementById('imgThree')];
+
+Image.imgOneEl = document.getElementById('imgOne');
+Image.imgTwoEl = document.getElementById('imgTwo');
+Image.imgThreeEl = document.getElementById('imgThree');
+Image.main = document.getElementById('main');
+Image.finalList = document.getElementById('finalList');
+
+Image.all = [];
+
+Image.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+for(var i = 0; i < Image.allNames.length; i++) {
+  new Image(Image.allNames[i]);
+}
+
+
+
+// event handler
+
+function randomImage(e) {
+
+
+  // add to the number questions answered
+  Image.numQuestionsAnswered++;
+
+  // when 25 clicks are reached
+  if(Image.numQuestionsAnswered === 26) {
+    var a = 0;
+    for(var i = 0; i < Image.allNames.length; i++) {
+      a = document.createElement('li');
+      a.textContent = Image.all[i].name + ' Clicked: ' + Image.all[i].timesClicked + ' Shown: ' + Image.all[i].timesShown;
+      Image.finalList.appendChild(a);
+    }
+    return;
+  }
+
+
+  // add to the array of which image is clicked
+  for(var i = 0; i < Image.all.length; i++) {
+    if(Image.all[i].name === e.target.alt) {
+      Image.all[i].timesClicked++;
+      console.log('value added');
+    }
+  }
+
+  // load three photos
+  for(var i = 0; i < Image.names.length; i++) {
+    var randomIndex = Image.prototype.randomIndex();
+    if (Image.all[randomIndex].name !== Image.names[0].alt && Image.all[randomIndex].name !== Image.names[1].alt) {
+      Image.names[i].src = Image.all[randomIndex].source;
+      Image.names[i].alt = Image.all[randomIndex].name;
+      Image.all[randomIndex].timesShown += 1;
+    } else {
+      i--;
+    }
+  }
+
+}
+
+
+
+
+// event listeners
+for(var i = 0; i < Image.names.length; i++) {
+  Image.names[i].addEventListener('click', randomImage);
+}
+
+
+
+
+// initial function
+function init() {
+  for(var i = 0; i < Image.names.length; i++) {
+    var randomIndex = Image.prototype.randomIndex();
+    if (Image.all[randomIndex].name !== Image.names[0].alt && Image.all[randomIndex].name !== Image.names[1].alt) {
+      Image.names[i].src = Image.all[randomIndex].source;
+      Image.names[i].alt = Image.all[randomIndex].name;
+      Image.all[randomIndex].timesShown += 1;
+    } else {
+      i--;
+    }
+  }
+}
+
+init();
